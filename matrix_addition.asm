3a56a6c3d41ec8e3a47f77f5fabcdd5f0343d89c
Avinash Prasad <avinash.mca17.du@gmail.com>
2017-11-12 23:24:02 +0530

    matrix addition using two variables

--- matrix_addition.asm
+++ matrix_addition.asm
@@ -16,30 +16,27 @@ code segment
 
     start:    
     mov ax, data
-    mov ds, ax
+    mov ds, ax                   ;load ds with "data"
     
     mov si,0
     mov di,0
     mov ax,0
-    mov bx,0
-    mov cx,three
+    mov bx,0                     ;clear registers
+    mov cx,three                 ;load cx with number of rows
     loop1: 
     mov si,0
-    push cx
-    mov cx,three
+    push cx                      ;push outer loop's cx onto stack
+    mov cx,three                 ;load cx with number of columns
         loop2:     
          mov ax,matrix1[bx][si]
-         add ax,matrix2[bx][si]
-         mov result[bx][si], ax
+         add ax,matrix2[bx][si]  ;add each element of both matrices
+         mov result[bx][si], ax  ;store sum in "result" matrix
          inc si           
-         inc si
+         inc si                  ;increment si
          loop loop2
-    add bx,six
-    pop cx
+    add bx,six                   ;increment bx for next row
+    pop cx                       ;load cx with outer loop's value
     loop loop1
     
 code ends 
     end start
-
-    
-    
